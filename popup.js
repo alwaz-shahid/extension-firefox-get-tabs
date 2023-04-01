0233f4a6e942c51dc9fd6bda0487b912c4d390e1
Alwaz Shahid <52915358+Alwaz18@users.noreply.github.com>
2023-04-01 19:58:42 +0500

    working

--- popup.js
+++ popup.js
@@ -1,4 +1,3 @@
-// Get all open tabs and their details
 chrome.tabs.query({}, function(tabs) {
   // Create an empty string to store the file contents
   var fileContent = "";
@@ -16,8 +15,8 @@ chrome.tabs.query({}, function(tabs) {
     document.getElementById('tabs-list').appendChild(li);
   }
 
-  // When the "Save to file" button is clicked, save the file to the user's computer
-  document.getElementById('save-btn').addEventListener('click', function() {
+  // When the "Save as file" button is clicked, save the file to the user's computer
+  document.getElementById('save-file-btn').addEventListener('click', function() {
     // Use the chrome.fileSystem API to create a new file
     chrome.fileSystem.chooseEntry({type: 'saveFile'}, function(writableEntry) {
       // Use the chrome.downloads API to download the file to the user's computer
@@ -34,7 +33,7 @@ chrome.tabs.query({}, function(tabs) {
   });
 
   // When the "Copy links and titles" button is clicked, copy the file contents to the clipboard
-  document.getElementById('copy-btn').addEventListener('click', function() {
+  document.getElementById('copy-links-btn').addEventListener('click',function() {
     var textarea = document.createElement('textarea');
     textarea.value = fileContent;
     document.body.appendChild(textarea);
@@ -43,4 +42,37 @@ chrome.tabs.query({}, function(tabs) {
     document.body.removeChild(textarea);
     alert('Links and titles copied to clipboard!');
   });
+
+  // Add styles to the UI
+  var styles = `body { font-family: Arial, sans-serif; } h1 { font-size: 24px; font-weight: bold; margin-bottom: 20px; } li { font-size: 18px; line-height: 1.5; margin-bottom: 10px; } button { background-color: #4CAF50; border: none; color: white; padding: 10px; text-align: center; text-decoration: none; display: inline-block; font-size: 16px; margin-right: 10px; border-radius: 5px; cursor: pointer; } #save-file-btn { background-color: #2196F3; } #copy-links-btn { background-color: #f44336; }`;
+
+  var styleSheet = document.createElement('style');
+  styleSheet.type = 'text/css';
+  styleSheet.innerText = styles;
+  document.head.appendChild(styleSheet);
 });
+
+// HTML code for the popup UI
+var html = `
+  <!DOCTYPE html>
+  <html>
+    <head>
+      <meta charset="UTF-8" />
+      <title>Open Tabs</title>
+    </head>
+    <body>
+      <h1>Open Tabs</h1>
+      <ul id="tabs-list"></ul>
+      <button id="save-file-btn">Save as file</button>
+      <button id="copy-links-btn">Copy links and titles</button>
+      <script src="popup.js"></script>
+    </body>
+  </html>
+`;
+
+// Set the HTML code for the popup UI
+chrome.browserAction.setPopup({popup: `data:text/html,${html}`});
+
+// /* h1 { font-size: 12px; font-weight: 700; margin-bottom: 8px; } ul {
+// list-style-type: none; padding: 0; margin: 0; } li { font-size: 12px;
+// margin-bottom: 4px; /* border: 2px red solid; */ list-style-type: disc; }
