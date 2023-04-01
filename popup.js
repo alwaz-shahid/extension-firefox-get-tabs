12fe5eccb94c8277bb5ae8c7c4d33ba7b07baef9
Alwaz Shahid <52915358+Alwaz18@users.noreply.github.com>
2023-04-01 17:36:56 +0500

    init

--- popup.js
+++ popup.js
@@ -1,47 +1,30 @@
-// Get all open tabs and their details
-chrome.tabs.query({}, function(tabs) {
-    // Create an empty string to store the file contents
-    var fileContent = "";
-  
-    // Loop through the tabs array and add each tab's title and URL to the file contents
+function saveToFile() {
+  chrome.tabs.query({}, function(tabs) {
+    var tabData = "";
     for (var i = 0; i < tabs.length; i++) {
-      var tab = tabs[i];
-      var title = tab.title;
-      var url = tab.url;
-      fileContent += title + ":\n" + url + "\n\n";
-  
-      // Add the tab title and URL to the UI
-      var li = document.createElement('li');
-      li.innerText = title + " - " + url;
-      document.getElementById('tabs-list').appendChild(li);
+      tabData += tabs[i].title + "\t" + tabs[i].url + "\n";
     }
-  
-    // When the "Save to file" button is clicked, save the file to the user's computer
-    document.getElementById('save-btn').addEventListener('click', function() {
-      // Use the chrome.fileSystem API to create a new file
-      chrome.fileSystem.chooseEntry({type: 'saveFile'}, function(writableEntry) {
-        // Use the chrome.downloads API to download the file to the user's computer
-        writableEntry.createWriter(function(writer) {
-          var blob = new Blob([fileContent], {type: 'text/plain'});
-          writer.write(blob);
-          chrome.downloads.download({
-            url: URL.createObjectURL(blob),
-            filename: writableEntry.name,
-            saveAs: false
-          });
-        });
-      });
-    });
-  
-    // When the "Copy links and titles" button is clicked, copy the file contents to the clipboard
-    document.getElementById('copy-btn').addEventListener('click', function() {
-      var textarea = document.createElement('textarea');
-      textarea.value = fileContent;
-      document.body.appendChild(textarea);
-      textarea.select();
-      document.execCommand('copy');
-      document.body.removeChild(textarea);
-      alert('Links and titles copied to clipboard!');
-    });
+
+    var blob = new Blob([tabData], {type: "text/plain;charset=utf-8"});
+    saveAs(blob, "tabs.txt");
   });
-  
+}
+
+function copyLinks() {
+  chrome.tabs.query({}, function(tabs) {
+    var links = "";
+    for (var i = 0; i < tabs.length; i++) {
+      links += tabs[i].url + "\n";
+    }
+
+    var dummy = document.createElement("textarea");
+    document.body.appendChild(dummy);
+    dummy.value = links;
+    dummy.select();
+    document.execCommand("copy");
+    document.body.removeChild(dummy);
+  });
+}
+
+document.getElementById("saveToFile").addEventListener("click", saveToFile);
+document.getElementById("copyLinks").addEventListener("click", copyLinks);
